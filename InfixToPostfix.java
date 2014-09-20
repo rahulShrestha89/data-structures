b50f1de6ad8411a72c16eb5b5ccf4591e7f5858a
RAHUL SHRESTHA <rahul.shrestha@selu.edu>
2014-09-20 04:46:03 -0500

    changes

--- InfixToPostfix.java
+++ InfixToPostfix.java
@@ -7,70 +7,158 @@ public class InfixToPostfix {
 
 	// instantiate Stack class
 	static Stack operatorStack = new Stack();
-	
+
 	/**
 	 * Constructor
 	 */
 	public InfixToPostfix() {
-		
+
 	}
-	
+
 	/**
 	 * Checks the expressions for balanced parentheses, braces and brackets
 	 * @param infix
 	 * @return true if the equations are balanced
 	 */
 	public boolean checkEquation(String infix){
-		
+
 		operatorStack.reset();
 		for(int j=0;j<infix.length();j++){    // loop until the end of the line for a string (expression)
-		    // Check for parentheses
-		        if( infix.charAt(j)=='('){
-		            operatorStack.push(infix.charAt(j)); // push the parentheses to stack
-		        } 
-		        // Check for braces
-		        else if( infix.charAt(j)=='[' ){
-		            operatorStack.push(infix.charAt(j)); // push the braces to stack
-		        }
-		        // Check for bracket
-		        else if( infix.charAt(j)=='{' ){
-		            operatorStack.push(infix.charAt(j)); // push the bracket to stack
-		        }
+			// Check for parentheses
+			if( infix.charAt(j)=='('){
+				operatorStack.push(infix.charAt(j)); // push the parentheses to stack
+			} 
+			// Check for braces
+			else if( infix.charAt(j)=='[' ){
+				operatorStack.push(infix.charAt(j)); // push the braces to stack
+			}
+			// Check for bracket
+			else if( infix.charAt(j)=='{' ){
+				operatorStack.push(infix.charAt(j)); // push the bracket to stack
+			}
 
-		        /* for reverse cases */
-		        // Check for reverse parentheses
-		        else if( infix.charAt(j)==')' ){
-		            if(operatorStack.isStackEmpty()){
-		                return false;
-		            }
-		            if(operatorStack.pop()!='('){
-		                return false;
-		            }
-		        }
-		        // Check for reverse braces
-		        else if( infix.charAt(j)==']' ){
-		            if(operatorStack.isStackEmpty()){
-		                return false;
-		            }
-		            if(operatorStack.pop()!='['){
-		                return false;
-		            }
-		        }
-		        // Check for reverse bracket
-		        else if( infix.charAt(j)=='}' ){
-		            if(operatorStack.isStackEmpty()){
-		                return false;
-		            }
-		            if(operatorStack.pop()!='{'){
-		                return false;
-		            }
-		        }
-		            // ignore other characters for now
-		    } // close for loop
+			/* for reverse cases */
+			// Check for reverse parentheses
+			else if( infix.charAt(j)==')' ){
+				if(operatorStack.isStackEmpty()){
+					return false;
+				}
+				if(operatorStack.pop()!='('){
+					return false;
+				}
+			}
+			// Check for reverse braces
+			else if( infix.charAt(j)==']' ){
+				if(operatorStack.isStackEmpty()){
+					return false;
+				}
+				if(operatorStack.pop()!='['){
+					return false;
+				}
+			}
+			// Check for reverse bracket
+			else if( infix.charAt(j)=='}' ){
+				if(operatorStack.isStackEmpty()){
+					return false;
+				}
+				if(operatorStack.pop()!='{'){
+					return false;
+				}
+			}
+			// ignore other characters for now
+		} // close for loop
 
-		    return operatorStack.isStackEmpty();
+		return operatorStack.isStackEmpty();
 
 	}
+
+	/**
+	 * Performs postfix operation and returns value 
+	 * @param infix
+	 * @return postfix operation value 
+	 *
+	 * -'0' converts into int 
+	 * +'0' converts into char
+	 */
+	
+	public char postfixOperation(String infix) {
+		
+		operatorStack.reset();
+		int firstOperand=0;
+		int secondOperand=0;
+		for(int j=0;j<infix.length();j++){    // loop until the end of the line for a string (expression)
+			// Check for numbers 
+			if( infix.charAt(j)>='0' && infix.charAt(j)<='9' ){
+				operatorStack.push(infix.charAt(j));
+				
+			}
+			//check for '+'
+			else if( infix.charAt(j)=='+'){
+				firstOperand = (int) operatorStack.pop() -'0';	// pop first operand
+				secondOperand = (int) operatorStack.pop() -'0'; // pop second operand
+				operatorStack.push((char)((firstOperand + secondOperand) +'0'));
+			}
+			//check for '-'
+			else if( infix.charAt(j)=='-'){
+				firstOperand = (int) operatorStack.pop() -'0';	// pop first operand
+				secondOperand = (int) operatorStack.pop() -'0'; // pop second operand
+				operatorStack.push((char)(( -firstOperand + secondOperand  ) +'0'));
+			}
+			//check for '*'
+			else if( infix.charAt(j)=='*'){
+				firstOperand = (int) operatorStack.pop()-'0';	// pop first operand
+				secondOperand = (int) operatorStack.pop()-'0'; // pop second operand
+				operatorStack.push((char)((firstOperand * secondOperand) +'0'));
+			}
+			//check for '/'
+			else if( infix.charAt(j)=='/'){
+				firstOperand = (int) operatorStack.pop()-'0';	// pop first operand
+				secondOperand = (int) operatorStack.pop()-'0'; // pop second operand
+				operatorStack.push((char)((secondOperand / firstOperand ) +'0'));
+			}
+			
+		}        
+
+		return operatorStack.pop();
+	} // end of postfixOperation();
+
+	/**
+	 * Converts an infix expression to postfix expression
+	 * @param string
+	 */
+	public StringBuffer infixToPostfix(String infix) {
+
+		StringBuffer postfix = new StringBuffer();	// create a string buffer
+		for(int j=0;j<infix.length();j++){    // loop until the end of the line for a string (expression)
+			if(infix.charAt(j)>='0' && infix.charAt(j)<='9'){
+				postfix.append(infix.charAt(j));
+			}
+			else if(infix.charAt(j)=='+'){
+				operatorStack.push(infix.charAt(j));
+			}
+			else if(infix.charAt(j)=='-'){
+				operatorStack.push(infix.charAt(j));
+			}
+			else if(infix.charAt(j)=='*'){
+				operatorStack.push(infix.charAt(j));
+			}
+			else if(infix.charAt(j)=='/'){
+				operatorStack.push(infix.charAt(j));
+			}
+			else if(infix.charAt(j)==')'){
+				postfix.append(operatorStack.pop());
+			}
+			else if(infix.charAt(j)=='('){
+				System.out.print("");
+			}
+			
+		}
+		while(!operatorStack.isStackEmpty()){	// clear the stack until empty
+			postfix.append(operatorStack.pop());
+		}
+		return postfix;
+		
+	}	// end of infixToPostfix()
 	
 
-}
+}	// end of class
--- Stack.java
+++ Stack.java
@@ -8,87 +8,89 @@ public class Stack {
 
 	private char [] stackElements;  // Holds stack elements
 	private int top;   // Stack top pointer
-   
-   /**
+
+	/**
       Constructor.
       @param capacity The capacity of the stack.
-   */
-	
-   public Stack () {
-       stackElements = new char[2000];
-       top = 0;
-   }
-   
-   /**
+	 */
+
+	public Stack () {
+		stackElements = new char[2000];
+		top = 0;
+	}
+
+	/**
       The empty method checks for an empty stack.
       @return true if stack is empty.
-   */
-   
-   public boolean isStackEmpty() 
-   { 
-       return top == 0; 
-   }
-   
-   /**
+	 */
+
+	public boolean isStackEmpty() 
+	{ 
+		return top == 0; 
+	}
+
+	/**
       The push method pushes a value onto the stack.
-      @param x The value to push onto the stack.
+      @param value The value to push onto the stack.
 	  @exception StackOverflowException When the 
 		stack is full.
-   */
-   
-   public void push(char value) 
-   {
-       if (top == stackElements.length)  
-           throw new StackOverFlowException();
-       else
-       {
-    	   stackElements[top] = value;
-          top ++;           
-       }         
-   } // end of push()
-   
-   /** 
+	 */
+
+	public void push(char value) 
+	{
+		if (top == stackElements.length)  
+			throw new StackOverFlowException();
+		else
+		{
+			stackElements[top] = value;
+			top ++;           
+		}         
+	} // end of push()
+
+	/** 
       The pop method pops a value off the stack.
       @return The value popped.
 	  @exception EmptyStackException When the 
 		stack is empty.
-   */
-   
-   public int pop()
-   {
-       if (isStackEmpty())
-           throw new EmptyStackException();
-       else
-       {
-          top--;
-          return stackElements[top];
-       }
-   } // end of pop()
-   
-   /** 
+	 */
+
+	public char pop()
+	{
+		if (isStackEmpty())
+			throw new EmptyStackException();
+		else
+		{
+			top--;
+			return stackElements[top];
+		}
+	} // end of pop()
+
+	/** 
       The peek method returns the value at the
       top of the stack.
       @return value at top of the stack.
 		@exception EmptyStackException When the 
 		stack is empty.
-   */
-   
-   int peek()
-   {
-       if (isStackEmpty())
-           throw new EmptyStackException();
-       else
-       {
-           return stackElements[top-1];
-       }
-   } // end of peek()
-   
-   /**
-    * reset stack
-    */
-   
-   public void reset(){
-	   top = 0;
-   } // end of reset()
+	 */
+
+	char peek()
+	{
+		if (isStackEmpty())
+			throw new EmptyStackException();
+		else
+		{
+			return stackElements[top-1];
+		}
+	} // end of peek()
+
+	/**
+	 * reset stack
+	 */
+
+	public void reset(){
+		top = 0;
+	} // end of reset()
+
+	
 
 }
